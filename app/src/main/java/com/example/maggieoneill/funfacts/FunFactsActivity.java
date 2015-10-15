0c0d2c6e3604e1958388e7f2f04e713a1310ce02
MaggieONeill <margaretanneoneill@gmail.com>
2015-10-15 10:08:52 -0700

    Initial commit

---
+++ app/src/main/java/com/example/maggieoneill/funfacts/FunFactsActivity.java
@@ -0,0 +1,36 @@
+package com.example.maggieoneill.funfacts;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        //declare our view variables and assign views from layout file
+
+        final TextView factLabel = (TextView)findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //the button was clicked , so update the fact label with new fact
+                String fact = "Ostriches can run faster than horses.";
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+    }
+}
